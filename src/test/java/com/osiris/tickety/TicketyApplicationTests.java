ff39299741932ef38b39a31681fee126958808d5
Francis Lagueu <lagson6@gmail.com>
2024-06-01 13:32:48 -0700

    Initial commit

---
+++ src/test/java/com/osiris/tickety/TicketyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osiris.tickety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
